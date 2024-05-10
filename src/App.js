bd7057bcacbf00ec4b5aaec392d080dbdeaae83f
Jose Gonzalez <coding.tamalito@gmail.com>
2024-05-10 17:39:39 +0200

    add logic to send data from frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+
+
+  // add an expense
+  const form = <form onSubmit={(event) => {submitData(event)}}>
+    <input type="number" placeholder={'amount'} name={'amount'}/>
+    <input type="number" placeholder={'month'} name={'month'}/>
+    <input type="number" placeholder={'year'} name={'year'}/>
+    <input type="text" placeholder={'type'} name={'type'}/>
+    <input type="text" placeholder={'notes'} name={'notes'}/>
+    <button >Add Expense</button>
+
+  </form>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hello World</h1>
+      {form}
     </div>
   );
+}
+
+
+function submitData(event) {
+  event.preventDefault();
+  const urlData = createUrlParams(event.nativeEvent.srcElement);
+
+  fetch(`http://localhost:8080/addExpense`, {
+    method: "POST",
+    body: urlData,
+  }).then(res => {
+    console.log(res);
+
+    // get the data
+    res.json().then(data => {
+      console.log(data);
+    })
+  }).catch(err => console.error(err));
+} // end of submitdata
+
+function createUrlParams(form) {
+  // get the form data
+  const formData = new FormData(form);
+
+  // create the urlParams
+  const urlData = new URLSearchParams();
+  for (const pair of formData) {
+    console.log(pair[0], pair[1]);
+    urlData.append(pair[0], pair[1]);
+  }
+
+  return urlData;
 }
 
 export default App;
