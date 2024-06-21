22921e8de303a31e940684d8cc9a3af1265860c0
Jose Gonzalez <coding.tamalito@gmail.com>
2024-06-21 17:43:22 +0200

    add all the types to avoid spellig mistakes

---
+++ src/utils/types.js
@@ -0,0 +1,17 @@
+
+ const expensesTypes = {
+    ESSENTIAL_FOOD: "essential_food",
+    NON_ESSENTIAL_FOOD: "non_essential_food",
+    PARTY: "party",
+    PHONE: 'phone',
+    INSURANCE: 'insurance',
+    CASH: 'cash',
+    HOME: 'home',
+    GIFT: 'gift',
+    RECREATIONAL_PURCHASE: 'recreational_purchase',
+    RENT: 'rent',
+    INCOME: 'income',
+    OTHER: 'other'
+}
+
+ export default expensesTypes;
