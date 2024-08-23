ac865779e831eafbc25a9b668370aadbd60b6344
Jose Gonzalez <coding.tamalito@gmail.com>
2024-08-23 17:11:14 +0200

    Add second typescript file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import {BrowserRouter} from "react-router-dom";
 import BaseRoutes from "./routes/BaseRoutes";
+import React from "react";
 
 
 
 function App() {
   // how to change the title of the webpage
-  document.title = 'Spotify Party';
+  document.title = 'Expenses Manager';
 
   return (
       <BrowserRouter>
