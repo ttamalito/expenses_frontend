74b5e7e65d8bdb9620358918830f08f257b5920e
Jose Gonzalez <coding.tamalito@gmail.com>
2024-08-28 17:42:42 +0200

    Add types for the different budget types

---
+++ src/utils/typesBudgetTypeDeclaration.ts
@@ -0,0 +1,62 @@
+type typesBudgetTypeDeclaration = {
+    // all types should be number
+    cash
+        :
+        number,
+    clothes
+        :
+        number,
+    essential_food
+        :
+        number,
+    gift
+        :
+        number,
+    gym
+        :
+        number,
+    home
+        :
+        number,
+    income
+        :
+        number,
+    insurance
+        :
+        number,
+    investment
+        :
+        number,
+    medicine
+        :
+        number,
+    non_essential_food
+        :
+        number,
+    other
+        :
+        number,
+    party
+        :
+        number,
+    phone
+        :
+        number,
+    recreational_purchase
+        :
+        number,
+    rent
+        :
+        number,
+    savings
+        :
+        number,
+    university
+        :
+        number,
+    vacation
+        :
+        number
+}
+
+export default typesBudgetTypeDeclaration;
