0ec87e4fe9a2f3441866500b91c2f583ff8d0a05
Jose Gonzalez <coding.tamalito@gmail.com>
2024-08-28 17:42:14 +0200

    Add simple base style for buttons

---
+++ src/FluentStyles/baseButton.ts
@@ -0,0 +1,10 @@
+import {makeStyles, shorthands} from "@fluentui/react-components";
+
+const useButtonStyles = makeStyles({
+    button: {
+        backgroundColor: '#0a788e',
+        color: 'white',
+    }
+});
+
+export default useButtonStyles;
