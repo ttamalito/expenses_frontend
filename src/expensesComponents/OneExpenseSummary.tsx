b35c7b565632ed00d4f5d6067ee3e82f3d82f7fc
Jose Gonzalez <coding.tamalito@gmail.com>
2024-09-05 16:29:06 +0200

    change one expense summary

--- src/expensesComponents/OneExpenseSummary.tsx
+++ src/expensesComponents/OneExpenseSummary.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import OneExpenseSummaryTypeDeclaration
     from "./utils/types/OneExpenseSummaryType";
+import {Button} from "@fluentui/react-components";
+import ModifyExpenseForm
+    from "./ModifyExpenseForm";
 
 interface IExpense {
     expense: OneExpenseSummaryTypeDeclaration
@@ -8,12 +11,19 @@ interface IExpense {
 
 export default function OneExpenseSummary({expense}: IExpense) {
 
+    const [showModifyForm, setShowModifyForm] = React.useState(false);
+
+    const modifyExpenseButton = <Button onClick={() => {setShowModifyForm(true) }}>Modify Expense</Button>
+
 
     const total = `Amount: ${expense.amount}`;
     const type = `Type: ${expense.type}`;
     const notes = `Notes: ${expense.notes}`;
     const date = `Date: ${expense.date}`;
-    const div = <div>{total} {type} {notes} {date}</div>
+    const div = <div>{total} {type} {notes} {date} <br/> {modifyExpenseButton} </div>
     const id = expense._id;
-    return div;
+    return showModifyForm ? <ModifyExpenseForm expense={expense} setShowModifyForm={setShowModifyForm}/> : div;
 }
+
+
+
