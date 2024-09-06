3ce164b1257c0f66b6be0ed54f95c23bb535af22
Jose Gonzalez <coding.tamalito@gmail.com>
2024-09-06 14:14:04 +0200

    Closes #13

--- src/expensesComponents/ModifyExpenseForm.tsx
+++ src/expensesComponents/ModifyExpenseForm.tsx
@@ -8,7 +8,8 @@ interface IExpense {
 }
 
 export default function ModifyExpenseForm({expense, setShowModifyForm}: IExpense) {
-    const expenseForm =  <form>
+    const expenseForm =  <form onSubmit={(event) =>
+        modifyExpense(event, expense._id)} >
         <input type="text" placeholder={'amount'} name={'amount'} defaultValue={expense.amount}/>
         <br/>
         <input type="text" placeholder={'date'} name={'date'} defaultValue={expense.date}/>
@@ -26,7 +27,27 @@ export default function ModifyExpenseForm({expense, setShowModifyForm}: IExpense
 }
 
 
-function modifyExpense(event: React.FormEvent<HTMLFormElement>) {
+function modifyExpense(event: React.FormEvent<HTMLFormElement>, id: string) {
     event.preventDefault();
     console.log('modifying expense');
+    const formData = new FormData(event.currentTarget);
+
+    // create the urlParams
+    const urlData = new URLSearchParams();
+    for (const pair of formData) {
+        console.log(pair[0], pair[1]);
+        urlData.append(pair[0], pair[1].toString());
+    }
+    fetch('http://localhost:8080/expenses/modify' + '?id=' + id, {
+        method: 'POST',
+        redirect: 'follow',
+        body: urlData,
+        credentials: 'include'
+    }).then(response => {
+        if (response.ok) {
+            alert('Expense modified successfully - Reload page to see changes');
+        } else {
+            alert('There was an error modifying the expense - see backend logs');
+        }
+    })
 }
