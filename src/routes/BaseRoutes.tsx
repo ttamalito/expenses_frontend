0034ac571fa35db8f315f8bd00b28670ac5b7b37
Jose Gonzalez <coding.tamalito@gmail.com>
2024-09-28 11:30:16 +0200

    Change internal route

--- src/routes/BaseRoutes.tsx
+++ src/routes/BaseRoutes.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Route, Routes} from "react-router-dom";
 import Base from "../Base";
 import MonthExpenses from "../MonthExpenses";
-import SetUp from "../SetUp";
+import SetUp from "../budget/SetUp";
 import YearSummary from "../spent/YearSummary";
 
 export default function BaseRoutes() {
@@ -10,7 +10,7 @@ export default function BaseRoutes() {
     return (<Routes>
         <Route path='/' element={<Base />}/>
         <Route path={'/expensesMonth/:month/:year'} element={<MonthExpenses />} />
-        <Route path={'/setUp'} element={<SetUp />} />
+        <Route path={'/budget/setup'} element={<SetUp />} />
         <Route path={'/summary/:year'} element={<YearSummary />} />
     </Routes>);
 }
