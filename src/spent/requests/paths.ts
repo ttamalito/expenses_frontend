312f247bf12c9d8fcd672b52cdc8023ab1bbc4f8
Jose Gonzalez <coding.tamalito@gmail.com>
2024-09-29 21:57:48 +0200

    Add paths for the spent folder

---
+++ src/spent/requests/paths.ts
@@ -0,0 +1,7 @@
+import {BACKEND_URL} from "../../constants";
+
+export const fetchTotalEarnedInYearPath = (year: number) => `${BACKEND_URL}/income/total-earned?year=${year}`;
+
+export const fetchExpensesOfATypeForAYearPath = (year: number, type: string) => `${BACKEND_URL}/expenses/single-type?year=${year}&type=${type}`;
+
+export const fetchTotalSpentInYearPath = (year: number) => `${BACKEND_URL}/expenses/total-spent?year=${year}`;
