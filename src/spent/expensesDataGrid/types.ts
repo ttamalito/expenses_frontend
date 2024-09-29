573e461335c4efb48edfcf63de4a731371507e3d
Jose Gonzalez <coding.tamalito@gmail.com>
2024-09-29 08:06:51 +0200

    Add types and functionality to display the expenses in a table manner

---
+++ src/spent/expensesDataGrid/types.ts
@@ -0,0 +1,29 @@
+
+export type IAmount = {
+    label: number;
+    icon: JSX.Element;
+};
+
+export type LastUpdatedCell = {
+    label: string;
+    timestamp: number;
+};
+
+export type ITypeCell = {
+    label: string;
+    icon: JSX.Element;
+};
+
+export type IDateCell = {
+    label: string;
+    icon: JSX.Element;
+};
+
+
+export type IExpenseItem = {
+    id: string
+    amount: IAmount;
+    type: ITypeCell;
+    date: IDateCell;
+    lastUpdated: LastUpdatedCell;
+};
