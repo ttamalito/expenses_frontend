92002c6b450d7b2963e8fb778e716fd913eb76d4
Jose Gonzalez <coding.tamalito@gmail.com>
2024-09-30 17:53:50 +0200

    Modify the ResponseWrapper

--- src/wrappers/IResponseWrapper.tsx
+++ src/wrappers/IResponseWrapper.tsx
@@ -1,8 +1,18 @@
-
-
+/**
+ * Interface for the response wrapper
+ */
 export interface IResponseWrapper<T> {
     response: Response,
     data: T,
     error: Error | undefined,
     element: JSX.Element | undefined
+    alert: JSX.Element | undefined
+}
+
+export const defaultResponseWrapper: IResponseWrapper<any> = {
+    response: new Response(''),
+    data: undefined,
+    error: undefined,
+    element: undefined,
+    alert: undefined
 }
