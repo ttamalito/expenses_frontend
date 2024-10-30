4def1765599f5602240afad1bdf39c7b56c60c27
Jose Gonzalez <coding.tamalito@gmail.com>
2024-10-30 08:33:12 +0100

    Remove unused files

--- src/utils/clearFilterButton.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from '@fluentui/react-components';
-import useButtonStyles from '../FluentStyles/baseButton';
-
-
-const clearFilterButton = <Button>Clear Filter</Button>;
-
-    export default clearFilterButton;
