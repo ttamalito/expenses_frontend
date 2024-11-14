0fa6c5bbe98f6471188089c1f42fd2df27a64605
Jose Gonzalez <coding.tamalito@gmail.com>
2024-11-14 22:30:21 +0100

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import {BrowserRouter} from "react-router-dom";
 import BaseRoutes from "./routes/BaseRoutes";
 import React from "react";
 import FallBackRoutes from "./routes/FallBackRoutes";
-import {fallbackRoute} from "./routes/paths/fallbackRoutesPaths";
 
 
 function App() {
