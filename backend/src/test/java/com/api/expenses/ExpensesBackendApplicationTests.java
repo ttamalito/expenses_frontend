955a04897ad139000eea00e57a1d5b756531b039
Jose Gonzalez <coding.tamalito@gmail.com>
2024-11-21 17:09:10 +0100

    Add java to project 2

---
+++ backend/src/test/java/com/api/expenses/ExpensesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
