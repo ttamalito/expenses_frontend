d62667587ae29e99f7f67ecb3213186f2cd74e17
Jose Gonzalez <coding.tamalito@gmail.com>
2024-11-22 21:49:00 +0100

    Add dummy implementation of user

---
+++ backend/src/main/java/com/api/expenses/rest/models/User.java
@@ -0,0 +1,67 @@
+package com.api.expenses.rest.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+
+public class User implements UserDetails {
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email, String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return "password";
+    }
+
+    @Override
+    public String getUsername() {
+        return "user";
+    }
+
+
+}
