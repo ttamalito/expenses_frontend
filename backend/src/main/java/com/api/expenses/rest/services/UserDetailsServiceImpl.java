51c5aebbb2633d3f7e2ae81273fa8052887619f9
Jose Gonzalez <coding.tamalito@gmail.com>
2024-11-22 21:49:28 +0100

    Add dummy implementation of UserDetailsService.java

---
+++ backend/src/main/java/com/api/expenses/rest/services/UserDetailsServiceImpl.java
@@ -0,0 +1,16 @@
+package com.api.expenses.rest.services;
+
+import com.api.expenses.rest.models.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("user", "password", "email", "role");
+    }
+}
