622b618f7c0a527cc323d205278e9be15f4efc28
Jose Gonzalez <coding.tamalito@gmail.com>
2024-11-25 21:27:54 +0100

    Update incomes

--- backend/src/main/java/com/api/expenses/rest/controllers/IncomesController.java
+++ backend/src/main/java/com/api/expenses/rest/controllers/IncomesController.java
@@ -1,24 +1,70 @@
 package com.api.expenses.rest.controllers;
 
+import com.api.expenses.rest.controllers.utils.ControllersHelper;
+import com.api.expenses.rest.services.IncomeService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/income")
 public class IncomesController {
-    @GetMapping("/total-earned")
-    public void getTotalEarnedForAYear(@RequestParam int year) {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final IncomeService incomeService;
+
+    @Autowired
+    public IncomesController(IncomeService incomeService) {
+        this.incomeService = incomeService;
+    }
+
+    @GetMapping("/total-earned") // TODO: rename the endpoint to /total-earned/year
+    public ResponseEntity<String> getTotalEarnedForAYear(@RequestParam int year) {
+        UUID userId = ControllersHelper.getUserIdFromSecurityContextHolder();
+
+        try {
+            float total = incomeService.getTotalEarnedForAYearForAUser(userId, year);
+            String response = String.format("{\"total\": %f}", total);
+            return ResponseEntity.ok(response);
+
+        } catch (Exception e) {
+            return ControllersHelper.handleException(e);
+        }
     }
      @GetMapping("/month")
-    public void getTotalEarnedInAMonth(@RequestParam int month, @RequestParam int year) {
+    public ResponseEntity<String> getTotalEarnedInAMonth(@RequestParam int month, @RequestParam int year) {
 
+        UUID userId = ControllersHelper.getUserIdFromSecurityContextHolder();
+        try {
+            float total = incomeService.getTotalEarnedForAMonthForAUser(userId, month, year);
+            String response = String.format("{\"total\": %f}", total);
+            return ResponseEntity.ok(response);
+
+        } catch (Exception e) {
+            return ControllersHelper.handleException(e);
+        }
      }
 
      @GetMapping("/year/monthly")
-    public void getTotalEarnedInAYearInAMonthlyBasis(@RequestParam int year) {
+    public ResponseEntity<String> getTotalEarnedInAYearInAMonthlyBasis(@RequestParam int year) {
 
+        UUID userId = ControllersHelper.getUserIdFromSecurityContextHolder();
+        try {
+            List<Float> totals = incomeService.getTotalEarnedInAYearInAMonthlyBasis(userId, year);
+            String totalsJson = objectMapper.writeValueAsString(totals);
+            return ResponseEntity.ok(totalsJson);
+        } catch (JsonProcessingException e) {
+            return ControllersHelper.handleException(e);
+        }
      }
+
+
 }
--- backend/src/main/java/com/api/expenses/rest/services/IncomeService.java
+++ backend/src/main/java/com/api/expenses/rest/services/IncomeService.java
@@ -2,11 +2,14 @@ package com.api.expenses.rest.services;
 
 import com.api.expenses.rest.models.Income;
 import com.api.expenses.rest.repositories.IncomeRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public class IncomeService {
 
     private final IncomeRepository incomeRepository;
@@ -41,6 +44,34 @@ public class IncomeService {
 
     public List<Income> getIncomesForAYearOfAUser(UUID userId, int year) {
         return incomeRepository.findByUserIdAndYear(userId, year);
+    }
+
+    public float getTotalEarnedForAYearForAUser(UUID userId, int year) {
+        List<Income> incomes = getIncomesForAYearOfAUser(userId, year);
+        float total = 0;
+        for (Income income : incomes) {
+            total += income.getAmount();
+        }
+        return total;
+    }
+
+    public float getTotalEarnedForAMonthForAUser(UUID userId, int month, int year) {
+        List<Income> incomes = getIncomesForAMonthOfAUser(userId, month, year);
+        float total = 0;
+        for (Income income : incomes) {
+            total += income.getAmount();
+        }
+        return total;
+    }
+
+    public List<Float> getTotalEarnedInAYearInAMonthlyBasis(UUID userId, int year) {
+        List<Float> totals = new ArrayList<>();
+        for (int i = 1; i <= 12; i++) {
+            float total = getTotalEarnedForAMonthForAUser(userId, i, year);
+            totals.add(total);
+//            System.out.println("Month: " + i + " Total: " + total);
+        }
+        return totals;
     }
 
     public List<Income> getIncomesForAMonthOfAUserByCategory(UUID userId, int month, int year, int categoryId) {
