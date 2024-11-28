86f43cff09ad64690f6e0c793d8991d237b621f3
Jose Gonzalez <coding.tamalito@gmail.com>
2024-11-28 08:00:14 +0100

    Add Native query to expese repository

--- backend/src/main/java/com/api/expenses/rest/repositories/ExpenseRepository.java
+++ backend/src/main/java/com/api/expenses/rest/repositories/ExpenseRepository.java
@@ -1,7 +1,10 @@
 package com.api.expenses.rest.repositories;
 
 import com.api.expenses.rest.models.Expense;
+import org.hibernate.annotations.NamedNativeQuery;
+import org.hibernate.annotations.NamedQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
@@ -12,6 +15,7 @@ public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
 
     public List<Expense> findByUserIdAndYear(UUID userId, int year);
 
+    @Query(value = "SELECT * FROM expenses WHERE user_id = ?1 AND month = ?2 AND year = ?3 AND category_id = ?4", nativeQuery = true)
     public List<Expense> findByUserIdAndMonthAndYearAndCategoryId(UUID userId, int month, int year, int categoryId);
 
     public List<Expense> findByUserIdAndWeekAndYear(UUID userId, int week, int year);
