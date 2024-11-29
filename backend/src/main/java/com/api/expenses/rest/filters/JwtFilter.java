185b91eabe42b82bfcab74086e0d4fb0e37f654e
Jose Gonzalez <coding.tamalito@gmail.com>
2024-11-29 08:11:19 +0100

    Change logging level

--- backend/src/main/java/com/api/expenses/rest/filters/JwtFilter.java
+++ backend/src/main/java/com/api/expenses/rest/filters/JwtFilter.java
@@ -45,7 +45,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        LOG.error("Processing authentication for '{}'", request.getRequestURL());
+        LOG.info("Processing authentication for '{}'", request.getRequestURL());
 
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
