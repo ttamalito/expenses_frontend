74a52d5cc03e9cc9c3bfa56e288e393f332ef0ff
Jose Gonzalez <coding.tamalito@gmail.com>
2024-12-03 22:20:15 +0100

    Add user model in the client

---
+++ ui/src/profile/models/User.ts
@@ -0,0 +1,11 @@
+
+type User = {
+    id: number;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    profilePicture: string;
+    monthlyBudget: number;
+    currency: Currency;
+}
