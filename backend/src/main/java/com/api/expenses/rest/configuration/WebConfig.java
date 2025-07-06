d5726096d8df8b71e99412503a65cedbfefb387c
Jose Gonzalez <coding.tamalito@gmail.com>
2025-07-07 00:52:15 +0200

    Add port of npm run preview for CORS

--- backend/src/main/java/com/api/expenses/rest/configuration/WebConfig.java
+++ backend/src/main/java/com/api/expenses/rest/configuration/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://localhost:4173")
                 .allowedMethods(
                         "GET",
                         "POST",
